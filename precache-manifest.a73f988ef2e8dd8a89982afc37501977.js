64b9ee99f46867fd764b167cf52b2cfc3074191f
Gert Vercruyssen <gert@cross-dream.co.jp>
2021-11-01 16:33:29 +0900

    Updates

---
+++ precache-manifest.a73f988ef2e8dd8a89982afc37501977.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c57eb93a66675bc490b97b2647f4e91",
+    "url": "/mipan/index.html"
+  },
+  {
+    "revision": "88e3e38fc2f0ff8ffa38",
+    "url": "/mipan/static/css/main.205f64a2.chunk.css"
+  },
+  {
+    "revision": "450773e146dff3672a65",
+    "url": "/mipan/static/js/2.29439fcb.chunk.js"
+  },
+  {
+    "revision": "88e3e38fc2f0ff8ffa38",
+    "url": "/mipan/static/js/main.bad3ccb1.chunk.js"
+  },
+  {
+    "revision": "ddb2aba47e3a74d6bef0",
+    "url": "/mipan/static/js/runtime-main.6948a752.js"
+  },
+  {
+    "revision": "83e91cee23f5ea052480ebb190bafcd8",
+    "url": "/mipan/static/media/4.83e91cee.png"
+  },
+  {
+    "revision": "96e631f0dda6b8e50b25be051ccbe31e",
+    "url": "/mipan/static/media/5.96e631f0.png"
+  }
+]);
